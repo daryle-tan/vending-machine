a97fcfb5063af1fcfeaefc1aa3f7521b53a2def6
daryle <daryle.tan@gmail.com>
2023-08-06 20:44:11 -0500

    purchaseSnack fn fin

--- contracts/VendingMachine.sol
+++ contracts/VendingMachine.sol
@@ -2,48 +2,51 @@
 pragma solidity ^0.8.18;
 
 contract VendingMachine {
+    // State Variables
     // use address(this).balance to find the contract's balance
     address private immutable i_owner;
-
+    
     struct Snacks {
         uint256 quantity;
         uint256 price;
     }
     mapping(string => Snacks) public inventory;
-
+    // tracks balances of each caller that interacts with the contract
+    mapping(address => uint256) public balances;
+    // Modifier
     modifier onlyOwner() {
         require(msg.sender == i_owner, "Not contract owner");
         _;
     }
 
-    constructor(uint256 initialQuantity, uint256 initialPrice) payable {
+    constructor() payable {
         i_owner = msg.sender;
-        inventory["chips"] = Snacks(initialQuantity, initialPrice);
-        inventory["drinks"] = Snacks(initialQuantity, initialPrice);
-        inventory["cookies"] = Snacks(initialQuantity, initialPrice);
+        inventory["chips"] = Snacks(20, 0.001 ether);
+        inventory["drinks"] = Snacks(20, 0.002 ether);
+        inventory["cookies"] = Snacks(20, 0.001 ether);
     }
 
     // Function that takes in ether payment in exchange for specific snack
-    function transaction(
+    function purchaseSnack(
         string calldata _snack,
         uint256 _quantity,
-        uint256 _price
     ) public payable {
-        require(msg.value >= .001 ether, "You must pay .001 for the snack");
-        // inventory[_snack] = new Snacks();
-        if (_snack == "chips") {
-            inventory["chips"] = Snacks(_quantity--, _price);
-        }
-        if (_snack == "drinks") {
-            inventory["drinks"] = Snacks(_quantity--, _price);
-        }
-        if (_snack == "cookies") {
-            inventory["cookies"] = Snacks(_quantity--, _price);
-        }
+        Snacks storage snack = inventory[_snack]
+        uint256 snackPrice = snack.price; // Retrieve the price based on the snack name
+        
+        require(snackPrice > 0, "Invalid snack selected"); // Verify that the snack price exists
+        require(msg.value >= snackPrice, "Declined! Insufficient payment amount."); // Verify that the payment is sufficient
+        
+        require(snack.quantity  >= _quantity, "We've sold out of this item!"); // Verify the quantity of snacks in the vending machine
+        snack.quantity -= _quantity;
+        
+        balances[msg.sender] += snackPrice; // accounts for the amount spent from customer
+        address(this).balance += snack.price; // accounts for revenue earned by vending machine
     }
 
     // Function to check the balance of the vending machine
     function getBalance() public view returns (uint256) {
+        // may have to add contractBalance = address(this.balance)
         return address(this).balance;
     }
 
