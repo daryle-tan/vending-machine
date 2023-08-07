ab7c738ef891d13216864625d054bb5da32c5c01
daryle <daryle.tan@gmail.com>
2023-08-06 22:32:08 -0500

    restock function fin and getBalance fn

--- contracts/VendingMachine.sol
+++ contracts/VendingMachine.sol
@@ -2,10 +2,17 @@
 pragma solidity ^0.8.18;
 
 contract VendingMachine {
-    // State Variables
+    /* Errors */
+    error VendingMachine__OverStock(string message);
+    /* State Variables */
     // use address(this).balance to find the contract's balance
+    uint256 public constant INITIAL_QUANTITY = 20;
+    uint256 contractBalance = address(this).balance;
+
     address private immutable i_owner;
-    
+
+    string[] private snackNames;
+
     struct Snacks {
         uint256 quantity;
         uint256 price;
@@ -13,7 +20,7 @@ contract VendingMachine {
     mapping(string => Snacks) public inventory;
     // tracks balances of each caller that interacts with the contract
     mapping(address => uint256) public balances;
-    // Modifier
+    /*  Modifier */
     modifier onlyOwner() {
         require(msg.sender == i_owner, "Not contract owner");
         _;
@@ -21,40 +28,65 @@ contract VendingMachine {
 
     constructor() payable {
         i_owner = msg.sender;
-        inventory["chips"] = Snacks(20, 0.001 ether);
-        inventory["drinks"] = Snacks(20, 0.002 ether);
-        inventory["cookies"] = Snacks(20, 0.001 ether);
+        inventory["chips"] = Snacks(INITIAL_QUANTITY, 0.001 ether);
+        inventory["drinks"] = Snacks(INITIAL_QUANTITY, 0.002 ether);
+        inventory["cookies"] = Snacks(INITIAL_QUANTITY, 0.001 ether);
+        snackNames.push("chips");
+        snackNames.push("drinks");
+        snackNames.push("cookies");
+    }
+
+    // fallback function handles unexpected function calls
+    fallback() external {
+        revert("Unexpected function call");
     }
 
     // Function that takes in ether payment in exchange for specific snack
-    function purchaseSnack(
-        string calldata _snack,
-        uint256 _quantity,
-    ) public payable {
-        Snacks storage snack = inventory[_snack]
+    function purchaseSnack(string calldata _snack, uint256 _quantity)
+        public
+        payable
+    {
+        Snacks storage snack = inventory[_snack];
         uint256 snackPrice = snack.price; // Retrieve the price based on the snack name
-        
+
         require(snackPrice > 0, "Invalid snack selected"); // Verify that the snack price exists
-        require(msg.value >= snackPrice, "Declined! Insufficient payment amount."); // Verify that the payment is sufficient
-        
-        require(snack.quantity  >= _quantity, "We've sold out of this item!"); // Verify the quantity of snacks in the vending machine
+        require(
+            msg.value >= snackPrice,
+            "Declined! Insufficient payment amount."
+        ); // Verify that the payment is sufficient
+
+        require(snack.quantity >= _quantity, "We've sold out of this item!"); // Verify the quantity of snacks in the vending machine
         snack.quantity -= _quantity;
-        
+
         balances[msg.sender] += snackPrice; // accounts for the amount spent from customer
-        address(this).balance += snack.price; // accounts for revenue earned by vending machine
+        contractBalance += snack.price; // accounts for revenue earned by vending machine
+    }
+
+    // Function to withdraw funds
+    function withdraw() public onlyOwner {}
+
+    // Function to restock the inventory
+    // only the owner should be able to call this fn
+    function restock() private onlyOwner {
+        // update quantity of snacks
+        for (uint256 i = 0; i < snackNames.length; i++) {
+            string memory snackName = snackNames[i];
+            Snacks storage snack = inventory[snackName];
+            uint256 restockQuantity = INITIAL_QUANTITY - snack.quantity;
+            if (restockQuantity >= INITIAL_QUANTITY) {
+                revert VendingMachine__OverStock("No need to restock");
+            }
+
+            if (snack.quantity < INITIAL_QUANTITY) {
+                snack.quantity += restockQuantity;
+            }
+        }
     }
 
     // Function to check the balance of the vending machine
     function getBalance() public view returns (uint256) {
-        // may have to add contractBalance = address(this.balance)
         return address(this).balance;
     }
-
-    // Function to restock the inventory
-    function restock() private {}
-
-    // Function to withdraw funds
-    function withdraw() public onlyOwner {}
 }
 
 // Layout of Contract:
