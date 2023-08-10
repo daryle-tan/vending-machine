5449490b98dfcb5fbfc53e510979da190774cf02
daryle <daryle.tan@gmail.com>
2023-08-09 20:27:05 -0500

    deploy script added

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+const ethers = require("ethers")
+require("dotenv").config()
+
+async function main() {
+  const VendingMachine = await ethers.getContractFactory("VendingMachine")
+  console.log("Deploying contract...")
+
+  const vendingMachine = await VendingMachine.deploy()
+  await vendingMachine.deployed()
+  console.log(`Deployed contract to: ${vendingMachine.address}`)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
