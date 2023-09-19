f07f968d7ad858c706c016df07e3ad1cd40eb762
daryle <daryle.tan@gmail.com>
2023-09-18 19:41:30 -0700

    VendingMachine started component

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head"
 import Image from "next/image"
 import { Inter } from "next/font/google"
 import styles from "@/styles/Home.module.css"
+import VendingMachine from "./VendingMachine"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -30,6 +31,7 @@ export default function Home() {
               priority
             />
           </div>
+          <VendingMachine />
         </div>
 
         <div className={styles.grid}></div>
---
+++ app/src/pages/vendingMachine.js
@@ -0,0 +1,11 @@
+import React from "react"
+import styles from "../styles/VendingMachine.module.css"
+function VendingMachine() {
+  return (
+    <>
+      <div className={styles.vendingContainer}>Here</div>
+    </>
+  )
+}
+
+export default VendingMachine
