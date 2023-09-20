8aa9c67c8a1867efcf680819cac171dacbba4d07
daryle <daryle.tan@gmail.com>
2023-09-19 19:24:16 -0700

    created vending machine container with components

--- app/src/pages/vendingMachine.js
+++ app/src/pages/vendingMachine.js
@@ -3,7 +3,48 @@ import styles from "../styles/VendingMachine.module.css"
 function VendingMachine() {
   return (
     <>
-      <div className={styles.vendingContainer}>Here</div>
+      <div className={styles.vendingContainer}>
+        <div className={styles.mainVending}>
+          <div className={styles.snackContainer}>
+            <div className={styles.snackLabel}>A</div>
+            <div className={styles.snackItem}>Chips</div>
+            <div className={styles.snackPrice}>0.001 ETH</div>
+            <div className={styles.snackQty}>QTY: 20</div>
+          </div>
+          <div className={styles.snackContainer}>
+            <div className={styles.snackLabel}>B</div>
+            <div className={styles.snackItem}>Drinks</div>
+            <div className={styles.snackPrice}>0.002 ETH</div>
+            <div className={styles.snackQty}>QTY: 20</div>
+          </div>
+          <div className={styles.snackContainer}>
+            <div className={styles.snackLabel}>C</div>
+            <div className={styles.snackItem}>Cookies</div>
+            <div className={styles.snackPrice}>0.001 ETH</div>
+            <div className={styles.snackQty}>QTY: 20</div>
+          </div>
+        </div>
+        <div className={styles.displayScreen}>
+          <div className={styles.totalAmount}>0.001 ETH</div>
+          <div className={styles.totalQty}>1</div>
+        </div>
+
+        <div className={styles.controlQty}>
+          <button className={styles.decrement}>-</button>
+          <div className={styles.controlLabel}>A</div>
+          <button className={styles.increment}>+</button>
+          <button className={styles.decrement}>-</button>
+          <div className={styles.controlLabel}>B</div>
+          <button className={styles.increment}>+</button>
+          <button className={styles.decrement}>-</button>
+          <div className={styles.controlLabel}>C</div>
+          <button className={styles.increment}>+</button>
+        </div>
+
+        <div className={styles.Pay}>
+          <button className={styles.payButton}>Pay</button>
+        </div>
+      </div>
     </>
   )
 }
